191f41a1dcd1aea54223d18242426f5c8fbc4ee3
viniciusnevescosta <vinicius.nevesco@gmail.com>
2025-01-28 18:00:20 -0300

    chore: Base project structure

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
+
+// https://astro.build/config
+export default defineConfig({
+	vite: { plugins: [tailwindcss()] },
+})
